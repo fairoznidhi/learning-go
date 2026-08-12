23822307434ff31e2ebcb37b2b99e268db1c9abc
fairoznidhi <fairoz1697@gmail.com>
2026-08-12 21:28:03 +0600

    chapter4 exercise fix

--- Book/ch4/exercise/3/main.go
+++ Book/ch4/exercise/3/main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	var total int
-	for i:=0; i<10;i++{
-		total:= total + i // total is shadowed everytime in the loop, so it will always be 0 + i
+	for i := 0; i < 10; i++ {
+		total := total + i // total is shadowed everytime in the loop, so it will always be 0 + i
+		fmt.Println(total)
 	}
 }
